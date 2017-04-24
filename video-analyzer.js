6b63c50c27394b6c76ae5eeb80debee8013b6dc9
Benjamin Smedberg <benjamin@smedbergs.us>
2017-04-24 11:04:49 -0400

    Add features "refresh file list" and "go to frame"

--- video-analyzer.js
+++ video-analyzer.js
@@ -7,25 +7,30 @@ var f = _("f");
 var gCurrentFile = null;
 var gCurrentFrame;
 
-// get the list of frame directories
-window.fetch("..").then((r) => {
-  if (!r.ok) {
-    alert("Failed to fetch directory listing");
-    return undefined;
-  }
-  return r.text();
-}).then((t) => {
-  var r = new RegExp('<a href="([^"]+)\\.frames/">', 'g');
-  var m;
-  var s = _("video-file");
-  while ((m = r.exec(t)) !== null) {
-    var filename = m[1];
-    var o = document.createElement("option");
-    o.value = filename;
-    o.textContent = unescape(filename);
-    s.appendChild(o);
-  }
-});
+function get_files() {
+ // get the list of frame directories
+ window.fetch("..").then((r) => {
+   if (!r.ok) {
+     alert("Failed to fetch directory listing");
+     return undefined;
+   }
+   return r.text();
+ }).then((t) => {
+   var r = new RegExp('<a href="([^"]+)\\.frames/">', 'g');
+   var m;
+   var s = _("video-file");
+   s.innerHTML = '<option value="">(none)</option>';
+   while ((m = r.exec(t)) !== null) {
+     var filename = m[1];
+     var o = document.createElement("option");
+     o.value = filename;
+     o.textContent = unescape(filename);
+     s.appendChild(o);
+   }
+ });
+}
+get_files();
+_("refresh-files").addEventListener("click", get_files);
 
 _("video-file").addEventListener("change", function() {
   if (this.value == "") {
@@ -64,6 +69,9 @@ function go(d) {
 }
 
 _("controls").addEventListener("click", function(e) {
+  if (!(e.originalTarget instanceof HTMLButtonElement)) {
+    return;
+  }
   switch (e.originalTarget.id) {
   case "wayback":
     go(-120);
@@ -81,6 +89,14 @@ _("controls").addEventListener("click", function(e) {
     break;
   case "wayfwd":
     go(120);
+    break;
+  case "inputframe":
+  case "go":
+    var f = parseInt(_("inputframe").value);
+    if (!isNaN(f) && f > 0) {
+      gCurrentFrame = f;
+      setFrame();
+    }
     break;
   default:
     console.error("unexpected click event", e.originalTarget);
